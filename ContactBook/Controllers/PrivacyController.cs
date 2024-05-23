5a54a55555a0851103b17f1014b4bd8140861db4
Ratnesh <ratcancode@gmail.com>
2024-05-24 01:18:36 +0530

    Added privacy page and released version 1.1

---
+++ ContactBook/Controllers/PrivacyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactBook.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
